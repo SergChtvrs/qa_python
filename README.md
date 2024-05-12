8ad3f211c221c9c4aae9752509ac829ea22c1c3a
Sergey Chtvrs <sa4zxc@gmail.com>
2024-05-12 13:16:32 +0300

    changed tests.py, readme

--- README.md
+++ README.md
@@ -3,11 +3,10 @@ ___
 
 Реализованные тесты в классе ```TestBooksCollector``` :
 
-+ ```test_add_new_book_with_param_length_successfully_added``` - проверяем, что книга добавлена, если ее название длиной 1, 10, 40 символов
++ ```test_add_new_book_with_param_length_successfully_added``` - проверяем, что книга добавлена, если ее название длиной 1, 40 символов
 + ```test_add_new_book_with_param_length_not_added``` - проверяем, что книга не добавлена, если ее название длиной 0, 41, 50 символов
 + ```test_add_new_book_add_book_already_in_list_not_added``` - проверяем что книга не добавляется, если она уже есть в коллекторе
 + ```test_set_book_genre_set_horror_genre``` - проверяем установку жанра ужасы
 + ```test_get_books_with_specific_genre_get_two_horror_books``` - проверяем получение 2-x книг в жанре ужасы
-+ ```test_get_books_for_children_get_three_books_cartoon_and_fantasy_genre``` - проверяем получение 3-x подходящие для детей книг в нескольких жанрах: мультфильм, фантастика
-+ ```test_add_book_in_favorites_add_two_books``` - проверяем добавление 2-х книг в Избранное
-+ ```test_delete_book_from_favorites_delete_one_of_two_books``` - проверяем удаление книги из Избранного
++ ```test_get_books_for_children_get_three_books_cartoon_and_fantasy_genre``` - проверяем получение подходящих для детей книг в разных жанрах
++ ```test_delete_book_from_favorites_delete_one_of_two_books``` - проверяем добавление 2-х книг в Избранное и удаление 1-й
--- tests.py
+++ tests.py
@@ -6,12 +6,11 @@ class TestBooksCollector:
     # проверяем, что книга добавлена, если ее название длиной 1, 10, 40 символов
     @pytest.mark.parametrize('book_name',
                              ['А',
-                              'Американск',
                               'Американский психопатАмериканский психоп'],
-                             ids=['длина 1 символ', 'длина 10 символов', 'длина 40 символов'])
+                             ids=['длина 1 символ', 'длина 40 символов'])
     def test_add_new_book_with_param_length_successfully_added(self, book_name, bookscollector):
         bookscollector.add_new_book(book_name)
-        assert book_name in bookscollector.books_genre
+        assert book_name in bookscollector.get_books_genre()
 
     # проверяем, что книга не добавлена, если ее название длиной 0, 41, 50 символов
     @pytest.mark.parametrize('book_name',
@@ -21,44 +20,38 @@ class TestBooksCollector:
                              ids=['пустое значение', 'длина 41 символ', 'длина 50 символов'])
     def test_add_new_book_with_param_length_not_added(self, book_name, bookscollector):
         bookscollector.add_new_book(book_name)
-        assert not book_name in bookscollector.books_genre
+        assert not book_name in bookscollector.get_books_genre()
 
     # проверяем что книга не добавляется, если она уже есть в коллекторе
     def test_add_new_book_add_book_already_in_list_not_added(self, bookscollector):
-        bookscollector.add_new_book('Приключения Шерлока Холмса')
-        key_lst = []
-        for key in bookscollector.books_genre.keys():
-            if key == 'Приключения Шерлока Холмса':
-                key_lst.append(key)
-        assert len(key_lst) == 1 and bookscollector.books_genre['Приключения Шерлока Холмса'] == 'Детективы'
+        dist_len = len(bookscollector.get_books_genre())
+        new_book = 'Приключения Шерлока Холмса'
+        bookscollector.add_new_book(new_book)
+        assert (len(bookscollector.get_books_genre()) == dist_len and
+                bookscollector.get_book_genre(new_book) == 'Детективы')
 
     # проверяем установку жанра ужасы
     def test_set_book_genre_set_horror_genre(self, bookscollector):
-        bookscollector.add_new_book('Зов Ктулху')
-        bookscollector.set_book_genre('Зов Ктулху', 'Ужасы')
-        assert bookscollector.get_book_genre('Зов Ктулху') == 'Ужасы'
+        new_book = 'Зов Ктулху'
+        genre = 'Ужасы'
+        bookscollector.add_new_book(new_book)
+        bookscollector.set_book_genre(new_book, genre)
+        assert bookscollector.get_book_genre(new_book) == genre
 
     # проверяем получение 2-x книг в жанре ужасы
     def test_get_books_with_specific_genre_get_two_horror_books(self, bookscollector):
-        assert bookscollector.get_books_with_specific_genre('Ужасы') == ['Падение Дома Ашеров', 'Молчание ягнят']
+        genre = 'Ужасы'
+        assert len(bookscollector.get_books_with_specific_genre(genre)) == 2
 
-    # проверяем получение 3-x подходящие для детей книг в жанрах Мультфильм, Фантастика
+    # проверяем получение подходящих для детей книг в разных жанрах
     def test_get_books_for_children_get_three_books_cartoon_and_fantasy_genre(self, bookscollector):
-        children_books_lst = []
-        for book in bookscollector.get_books_for_children():
-            if book in bookscollector.get_books_genre():
-                children_books_lst.append(book)
-        assert len(children_books_lst) == 3
+        assert len(bookscollector.get_books_for_children()) == 3
 
-    # проверяем добавление 2-х книг в Избранное
-    def test_add_book_in_favorites_add_two_books(self, bookscollector):
-        bookscollector.add_book_in_favorites('Приключения Шерлока Холмса')
-        bookscollector.add_book_in_favorites('Падение Дома Ашеров')
-        assert bookscollector.get_list_of_favorites_books() == ['Приключения Шерлока Холмса', 'Падение Дома Ашеров']
-
-    # проверяем удаление книги из Избранного
+    # проверяем добавление 2-х книг в Избранное и удаление 1-й
     def test_delete_book_from_favorites_delete_one_of_two_books(self, bookscollector):
-        bookscollector.add_book_in_favorites('Приключения Шерлока Холмса')
-        bookscollector.add_book_in_favorites('Падение Дома Ашеров')
-        bookscollector.delete_book_from_favorites('Приключения Шерлока Холмса')
-        assert bookscollector.get_list_of_favorites_books() == ['Падение Дома Ашеров']
+        book_1 = list(bookscollector.get_books_genre())[0]
+        book_2 = list(bookscollector.get_books_genre())[1]
+        bookscollector.add_book_in_favorites(book_1)
+        bookscollector.add_book_in_favorites(book_2)
+        bookscollector.delete_book_from_favorites(book_1)
+        assert bookscollector.get_list_of_favorites_books() == [book_2]
